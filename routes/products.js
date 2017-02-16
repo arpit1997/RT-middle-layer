17af1e7eeb37cba6fcdf06b87645e64a493cccbb
arpit1997 <solankiarpit1997@gmail.com>
2017-02-16 12:38:05 +0530

    feat: implement offset and limit params

--- routes/products.js
+++ routes/products.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var request = require('request')
+var url = require('url');
 var parseString = require('xml2js').parseString;
 var products = require('../lib/products')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/:shop_id/products', function(req, res, next) {
-  var query_params = req.query;
-  var param_limit = req.query.limit
-  var param_offset = req.query.offset
-  products.convert_product_data(param_limit, param_offset, function (err, data) {
+  var url_parts = url.parse(req.url, true)
+  var limit = url_parts.query.limit
+  var offset = url_parts.query.offset
+  console.log(limit)
+  console.log(offset)
+  products.convert_product_data(limit, offset, function (err, data) {
     if (err){
       res.json({'status': '500'})
     }
@@ -27,14 +30,6 @@ router.get('/:shop_id/products/:product_id', function (req, res, next) {
     else {
       res.json(data)
     }
-  })
-})
-
-router.get('/4/shops', function(req, res){
-  var URL = "http://77.93.198.186/4/shops"
-  request(URL, function (err, response, body) {
-    var response = JSON.parse(body)
-    res.json(response)
   })
 })
 
