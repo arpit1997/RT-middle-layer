c39fc4d258eb906c6cf05520dca4355bd0ea699c
arpit1997 <solankiarpit1997@gmail.com>
2017-02-18 20:15:45 +0530

    feat: implemented sorting param in category wise product api

--- routes/products.js
+++ routes/products.js
@@ -46,6 +46,8 @@ router.get('/:shop_id/products', function (req, res, next) {
   var limit = url_parts.query.limit
   var offset = url_parts.query.offset
   var category = url_parts.query.category
+  var sorting = url_parts.query.sort
+  var records = []
   if (limit === undefined && offset === undefined) {
     limit = 8
     offset = 0
@@ -61,7 +63,28 @@ router.get('/:shop_id/products', function (req, res, next) {
         if (!err){
           products.get_category_wise_product(converted_data, category_sku, function (data) {
             products.limit_cat_data(data, limit, offset, category, function (cat_wise_data) {
-              res.json(cat_wise_data)
+              if (sorting === "price_ASC"){
+                records = cat_wise_data.records
+                records.sort(function (a,b) {
+                  return parseFloat(a.price) > parseFloat(b.price)
+                })
+                cat_wise_data.records = records
+                res.json(cat_wise_data)
+              }
+              else{
+                if (sorting === "price_DESC"){
+                  records = cat_wise_data.records
+                  records.sort(function (a,b) {
+                    return parseFloat(a.price) < parseFloat(b.price)
+                  })
+                  cat_wise_data.records = records
+                  res.json(cat_wise_data)
+                }
+                else {
+                  res.json(cat_wise_data)
+                }
+              }
+
             })
           })
         }
