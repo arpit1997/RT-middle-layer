46c7195c4a3077cabed0e2d71208562b49bd9da1
System <none>
2017-02-22 19:03:57 +0000

    fix: fixed login api

--- routes/authentication.js
+++ routes/authentication.js
@@ -10,8 +10,18 @@ var LOGIN_URL = 'https://rentoys.xyz/index.php/rest/V1/integration/customer/toke
 var USER_PROFILE_URL = 'https://rentoys.xyz/index.php/rest/V1/customers/me'
 
 router.post('/login/email', function (req, res, next) {
-  var username = req.body.username
-  var password = req.body.password
+
+  console.log(req.body)
+  var creds
+  for (var i in req.body){
+    creds = JSON.parse(i)
+  }
+  console.log(creds.email)
+  
+  var username = creds.email
+  var password = creds.password
+  console.log(username)
+  console.log(password)
   auth.gettoken_login_with_email(username, password, function (access_token) {
     auth.user_profile(access_token, function (data) {
       res.json(data)
